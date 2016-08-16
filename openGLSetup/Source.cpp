318569ea362b1d987ebf74651b383eba1e8ced86
Ben <Ben@BABYGIRL-THE-SECOND>
2016-08-16 15:24:33 -0700

    new break to fix color change bug

--- openGLSetup/Source.cpp
+++ openGLSetup/Source.cpp
@@ -84,8 +84,9 @@ void processSpecialKeys(int key, int x, int y)
 		{
 			red = 1.0;
 			green = 0.0;
-			blue = 0.0; break;
+			blue = 0.0; 
 		}
+		break;
 	case GLUT_KEY_F2:
 		red = 0.0;
 		green = 1.0;
