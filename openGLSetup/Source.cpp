b51325ec6544353584312a120eec68fb1c5c5093
Ben <Ben@BABYGIRL-THE-SECOND>
2016-08-16 15:13:12 -0700

    formatting

--- openGLSetup/Source.cpp
+++ openGLSetup/Source.cpp
@@ -67,10 +67,9 @@ void processSpecialKeys(int key, int x, int y)
 {
 	switch (key) {
 	case GLUT_KEY_F1:
-			red = 1.0;
-			green = 0.0;
-			blue = 0.0;
-		 break;
+		red = 1.0;
+		green = 0.0;
+		blue = 0.0;break;
 	case GLUT_KEY_F2:
 		red = 0.0;
 		green = 1.0;
